18769753b38e0abd02ff0507258be733661afb4b
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-07-05 14:58:17 +0530

    add design impelentation fles

---
+++ constants/icons.js
@@ -0,0 +1,13 @@
+import booking from "../assets/icons/book.png";
+import home from "../assets/icons/home.png";
+import profile from "../assets/icons/profile.png";
+import search from "../assets/icons/search.png";
+import wishlist from "../assets/icons/wishlist.png"
+
+export default {
+  home,
+  booking,
+  search,
+  profile,
+  wishlist
+};
